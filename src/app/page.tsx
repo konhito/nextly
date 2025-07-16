494948bbd6f3e5824abfa20fdef267ad34b77a3b
konhito <konhito02@gmail.com>
2025-07-16 16:11:36 +0530

    prisma-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { prisma } from "@/lib/db"
+import prisma from "@/lib/db"
 
 const page = async () => {
   const users = await prisma.user.findMany()
 
-  
+
   return (
     <div className="font-bold text-rose-500">
       Hello World
