9c7cd60bc3d0c3c4877029c4d92377c6fcef364d
konhito <konhito02@gmail.com>
2025-07-21 10:28:34 +0530

    finalization: overwriting openai baseURL to a4f.co

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -7,17 +7,24 @@ export const helloWorld = inngest.createFunction(
   async ({ event }) => {
     
     const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer, You summarize in 2 words",
-      model: openai({model: "gpt-4o", apiKey: process.env.OPENAI_API_KEY})
+      name: "code-agent",
+      system: "you are an expert nedt.js developer, you write readable, maintaainable code. you write simple Next.js & React snippets.",
+      model: openai({
+        model: "provider-3/gpt-4",
+        apiKey: process.env.OPENAI_API_KEY,
+        baseUrl: process.env.OPENAI_API_BASE
+      })
     });
 
-    const { output } = await summarizer.run(
-      `summarize the following text: ${event.data.value}`,
-    );
-
-    console.log(output);
-
-    return { output };
+    try {
+      const { output } = await summarizer.run(
+        `Write a code snippet: ${event.data.value}`,
+      );
+      console.log(output);
+      return { output };
+    } catch (error) {
+      console.error("AI Job Error:", error);
+      throw error;
+    }
   },
 );
